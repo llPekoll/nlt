0d691fa342279dc5e840823aa12d6422ecc22a25
peko <yoyo.mepa@gmail.com>
2022-06-22 21:17:45 +0200

    start to work on the card to mint

--- NLT/src/lib/Mint.svelte
+++ NLT/src/lib/Mint.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import NLTNFT from './NLTNFT.json';
+    import NTL1 from './0.json';
+    import NTL2 from './1.json';
+    import NTL3 from './2.json';
+    import MintCard from './MintCard.svelte';
 
     const nltContractAddress = '0x51B2876D671E96FAF773dC66239965bA9B9b7526'
 
@@ -23,8 +27,17 @@
                 console.log(contract)
                 // const res = await contract.withdraw();
                 const res = await contract.mintPrices(1);
+                const res2 = await contract.tiers(1);
+                const res3 = await contract.tiers(2);
+                const res4 = await contract.uri(2);
+                // const res3 = await contract.isPublicMintEnable();
                 console.log(7)
-                console.log('res:', res);
+                // ethers.utils.formatEther( value )
+                console.log('res:', ethers.utils.formatEther(res));
+                console.log('res:', ethers.utils.formatUnits(res2,0));
+                console.log('res:', ethers.utils.formatUnits(res3,0));
+                console.log('res:', res4);
+                // console.log('res:', res3);
             } catch (err) {
                 console.log('err:',err)
             }
@@ -33,6 +46,9 @@
 
 </script>
 <div class="flex items-center justify-center pt-20">
+    <MintCard nlt={NTL1} />
+    <MintCard nlt={NTL2} />
+    <MintCard nlt={NTL3} />
     <button on:click={handleMint} class=" py-2 px-10 bg-red-600 text-white rounded-lg">
         Mint
     </button>
---
+++ NLT/src/lib/MintCard.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let nlt;
+    const src = nlt.image;
+
+</script>
+
+<div>
+    <img {src} alt='nlt'/>
+
+</div>
