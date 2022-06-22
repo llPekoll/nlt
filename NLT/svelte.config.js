1615c55a3767b67af9a6b6bf755d3b9c9e298d20
peko <yoyo.mepa@gmail.com>
2022-06-22 22:35:17 +0200

    start to work on the card to mint

--- NLT/svelte.config.js
+++ NLT/svelte.config.js
@@ -1,13 +1,12 @@
 import preprocess from 'svelte-preprocess';
-import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	kit: {
-		adapter: node({ out: 'build' }),
-		vite: { optimizeDeps: { include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep'] } }
+		adapter: adapter()
 	},
 	preprocess: [
 		preprocess({
