abfe3eeb0e8913f14063d86276dcd7d241c62648
peko <yoyo.mepa@gmail.com>
2022-08-21 16:30:38 +0200

    push online

--- jango/jango/settings.py
+++ jango/jango/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('JANGO_SECRET_KEY', 'jose')
 DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app", "localhost",'https://nft-trades.com','https://www.nft-trades.com',"http://admin.nftl-trades.com" ]
+CSRF_TRUSTED_ORIGINS = ['http://admin.nftl-trades.com', "https://admin.nftl-trades.com"]
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # Application definition
