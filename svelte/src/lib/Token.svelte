273ddcb839e68b25a33497d73e606a60c4374064
peko <yoyo.mepa@gmail.com>
2022-08-27 18:20:26 +0200

    deploy on mainnet

--- svelte/src/lib/Token.svelte
+++ svelte/src/lib/Token.svelte
@@ -218,7 +218,7 @@
 <section class="flex items-center justify-center py-20">
 	<div class="w-4/5 flex rounded-lg shadow-lg shadow-white">
 		{#if nft}
-			<img src={nft.image} alt="nft" class="rounded-tl-lg rounded-bl-lg w-1/2" />
+			<img src={nft.image} alt="nft" class="rounded-tl-lg rounded-bl-lg" />
 			
 			<div class="bg-gray-100 rounded-tr-lg rounded-br-lg w-2/3 p-4 pl-6">
 				<p class="text-3xl font-bold italic capitalize pt-7">{nft.name}</p>
