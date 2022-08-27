6be7f6a0538d59dde413f363ffde82347eb9c194
peko <yoyo.mepa@gmail.com>
2022-08-27 10:50:40 +0200

    deploy on mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -30,18 +30,18 @@ module.exports = {
     hardhat: {
     },
     // testnet: {
-    mainnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      chainId: 97,
-      gasPrice: 20000000000,
-      accounts: {mnemonic: mnemonic}
-    },
     // mainnet: {
-    //   url: "https://bsc-dataseed.binance.org/",
-    //   chainId: 56,
+    //   url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+    //   chainId: 97,
     //   gasPrice: 20000000000,
     //   accounts: {mnemonic: mnemonic}
-    // }
+    // },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic}
+    }
   },
   solidity: {
     version: "0.8.4",
