22e382b8cb6ee3c9eb891b6fbc39bc164cabc3aa
peko <yoyo.mepa@gmail.com>
2022-08-29 07:42:14 +0200

    deploy on mainnet

--- svelte/src/lib/Share.svelte
+++ svelte/src/lib/Share.svelte
@@ -5,12 +5,16 @@
 <div class="flex items-center justify-center">
 	<div class="px-16">
 		<div class="h-16 w-16 fill-green-100 text-green-100">
+		<a href="https://discord.gg/JYzWHYxT">
 			{@html trad.twitter_svg}
+		</a>
 		</div>
 	</div>
 	<div class="px-16">
 		<div class="h-16 w-16 fill-green-100">
+		<a href="https://twitter.com/llpekoll">
 			{@html trad.discord_svg}
+		</a>
 		</div>
 	</div>
 </div>
